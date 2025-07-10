035ed96b4c8fcae2e32d2a92db53df9998b14227
Alwin Yesudas <alwin.yesudas@innovaturelabs.com>
2025-07-10 15:08:27 +0530

    Auto-create User on teacher/student add by admin

--- schoolMgmt/teachers/serializers.py
+++ schoolMgmt/teachers/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Teacher
+from .models import Student
+from users.models import User
 
-class TeacherSerializer(serializers.ModelSerializer):
+class StudentSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source='user.email', required=True)
+
     class Meta:
-        model = Teacher
-        fields = '__all__'
+        model = Student
+        fields = [
+            'id', 'first_name', 'last_name', 'email', 'phone_number',
+            'roll_number', 'class_name', 'date_of_birth', 'admission_date',
+            'status', 'assigned_teacher'
+        ]
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create_user(
+            username=user_data['email'],
+            email=user_data['email'],
+            password="student@123",
+            role='student'
+        )
+        student = Student.objects.create(user=user, **validated_data)
+        return student
