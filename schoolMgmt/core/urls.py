91876e8085d3685cc82da2ed6cfb3b60ae042ef6
Alwin Yesudas <alwin.yesudas@innovaturelabs.com>
2025-07-10 15:00:50 +0530

    Student view added

--- schoolMgmt/core/urls.py
+++ schoolMgmt/core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from teachers.views import TeacherViewSet
-from students.views import StudentViewSet
+from students.views import StudentViewSet, StudentMeView
 
 router = DefaultRouter()
 router.register(r'teachers', TeacherViewSet)
 router.register(r'students', StudentViewSet)
 
 urlpatterns = [
+    path('me/', StudentMeView.as_view(), name='student-me'),
     path('', include(router.urls)),
 ]
--- schoolMgmt/students/views.py
+++ schoolMgmt/students/views.py
@@ -2,6 +2,13 @@ from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer
 from users.permissions import IsAdminOrTeacher
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from users.models import User
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import status
 
 class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
@@ -14,3 +21,20 @@ class StudentViewSet(viewsets.ModelViewSet):
         elif user.role == 'teacher':
             return Student.objects.filter(assigned_teacher__user=user)
         return Student.objects.none()
+    
+class StudentMeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        if user.role != 'student':
+            return Response({"detail": "Only students can access this endpoint."}, status=status.HTTP_403_FORBIDDEN)
+
+        try:
+            student = Student.objects.get(user=user)
+        except Student.DoesNotExist:
+            return Response({"detail": "Student profile not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
